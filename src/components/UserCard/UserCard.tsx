ae3ea6e0ba93bc02b5fca170f8b02a3b1e232135
Ivan Davidyuk <vanyadavidyuk98@mail.ru>
2023-10-27 19:58:22 +0300

    project is done

---
+++ src/components/UserCard/UserCard.tsx
@@ -0,0 +1,38 @@
+import { UserInfo } from 'components/UserInfo';
+import { UserStat } from 'components/UserStat';
+import { UserTitle } from 'components/UserTitle';
+import { LocalGithubUser } from 'types';
+import styles from './UserCard.module.scss';
+
+interface UserCardProps extends LocalGithubUser { }
+
+export const UserCard = (props: UserCardProps) => {
+  return (
+    <div className={styles.userCard}>
+      <img
+        src={props.avatar}
+        alt={props.login}
+        className={styles.avatar}
+      />
+      <UserTitle
+        created={props.created}
+        login={props.login}
+        name={props.name}
+      />
+      <p className={`${styles.bio}${props.bio ? '' : ` ${styles.empty}`}`}>
+        {props.bio || 'This profile has no bio'}
+      </p>
+      <UserStat
+        repos={props.repos}
+        followers={props.followers}
+        following={props.following}
+      />
+      <UserInfo
+        blog={props.blog}
+        company={props.company}
+        location={props.location}
+        twitter={props.twitter}
+      />
+    </div>
+  );
+};
---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from 'types';
+
+export const defaultUser: LocalGithubUser = {
+  "login": "V4ne4ek",
+  "avatar": "https://avatars.githubusercontent.com/u/122744593?v=4",
+  "name": "Ivan Davidyuk",
+  "company": "",
+  "blog": "",
+  "location": "Moscow",
+  "bio": "React developer",
+  "twitter": "",
+  "repos": 11,
+  "followers": 0,
+  "following": 0,
+  "created": "2023-01-15T17:36:16Z",
+}
